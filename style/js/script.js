c9810ce671315f9f50b1806e0b05f76c75cdae0a
agiljibrin <agiljibrin009@gmail.com>
2022-12-27 14:14:57 +0800

    clear

--- style/js/script.js
+++ style/js/script.js
@@ -1,10 +1,3 @@
 
 // aos init enable
 AOS.init();
-
-let navbar = document.getElementsByClassName("navbar");
-console.log(navbar);
-
-document.getElementsByClassName("navbar").addEventListener("scroll", () => {
-    alert("hello");
-});
